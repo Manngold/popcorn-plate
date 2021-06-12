1c22ad23874c9a6762eb85f84cd12d2d9a9d6bf1
Manngold <jsw4820@gmail.com>
2021-06-12 17:23:36 +0900

    basic setting

---
+++ components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="container__footer">
+    <h1>here is footer</h1>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="container__header">
+    <h1>here is header</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
